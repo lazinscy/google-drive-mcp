4e14d8c7ba21f22f572df2ac784228c88b5ab0e8
dawidlazinski <dawid.lazinski@hotmail.com>
2026-01-08 23:03:09 +0100

    feat: add types for metadata and revisions

--- src/types.ts
+++ src/types.ts
@@ -34,3 +34,40 @@ export interface File {
   viewLink?: string | null
   downloadLink?: string | null
 }
+
+export interface FileMetadata {
+  id: string | null | undefined
+  name: string | null | undefined
+  mimeType: string | null | undefined
+  size: string | null | undefined
+  createdTime: string | null | undefined
+  modifiedTime: string | null | undefined
+  owners?: { displayName?: string | null; emailAddress?: string | null }[]
+  lastModifyingUser?: { displayName?: string | null; emailAddress?: string | null }
+  webViewLink: string | null | undefined
+  webContentLink: string | null | undefined
+  parents: string[] | null | undefined
+  shared: boolean | null | undefined
+  trashed: boolean | null | undefined
+}
+
+export interface GetFileMetadataResponse {
+  success: boolean
+  metadata?: FileMetadata
+  message?: string
+}
+
+export interface FileRevision {
+  id: string | null | undefined
+  mimeType: string | null | undefined
+  modifiedTime: string | null | undefined
+  lastModifyingUser?: { displayName?: string | null; emailAddress?: string | null }
+  size: string | null | undefined
+  originalFilename: string | null | undefined
+}
+
+export interface GetFileRevisionsResponse {
+  success: boolean
+  revisions?: FileRevision[]
+  message?: string
+}
